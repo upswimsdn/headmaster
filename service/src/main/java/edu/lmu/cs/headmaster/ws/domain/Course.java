380d65aa67fc403205a65fdad9362078f9e15ce8
Eric Jaso <ericjaso@gmail.com>
2013-02-21 22:34:06 -0800

    First stub of course domain object and SBGRubric.

---
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/domain/Course.java
@@ -0,0 +1,117 @@
+package edu.lmu.cs.headmaster.ws.domain;
+
+import java.util.List;
+
+import edu.lmu.cs.headmaster.ws.types.Term;
+
+public class Course {
+    private Long id;
+    private String classTitle;
+    private String description;
+    private String discipline;
+    private Integer credits;
+    private String room;
+    private Term term;
+    private Integer year;
+    private Integer classSize;
+    private String instructor;
+    private SBGRubric sbgRubric;
+    private List<Student> enrolledStudents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClassTitle() {
+        return classTitle;
+    }
+
+    public void setClassTitle(String classTitle) {
+        this.classTitle = classTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Term getTerm() {
+        return term;
+    }
+
+    public void setTerm(Term term) {
+        this.term = term;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getClassSize() {
+        return classSize;
+    }
+
+    public void setClassSize(Integer classSize) {
+        this.classSize = classSize;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public SBGRubric getSbgRubric() {
+        return sbgRubric;
+    }
+
+    public void setSbgRubric(SBGRubric sbgRubric) {
+        this.sbgRubric = sbgRubric;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(List<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+}
