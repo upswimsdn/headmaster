5373244c2b2368c53c57dfebf1a85616d392b61f
Eric Jaso <ericjaso@gmail.com>
2013-02-27 12:57:31 -0800

    Stubs for SBGAssignment, SBGOutcome, and filled out SBGRubric.

---
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/domain/SBGAssignment.java
@@ -0,0 +1,5 @@
+package edu.lmu.cs.headmaster.ws.domain;
+
+public class SBGAssignment {
+
+}
--- service/src/main/java/edu/lmu/cs/headmaster/ws/domain/SBGRubric.java
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/domain/SBGRubric.java
@@ -1,5 +1,24 @@
 package edu.lmu.cs.headmaster.ws.domain;
 
-public class SBGRubric {
+import java.util.List;
 
+public class SBGRubric {
+    private List<SBGOutcome> outcomes;
+    private List<SBGAssignment> assignments;
+
+    public List<SBGOutcome> getOutcomes() {
+        return outcomes;
+    }
+
+    public void setOutcomes(List<SBGOutcome> outcomes) {
+        this.outcomes = outcomes;
+    }
+
+    public List<SBGAssignment> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(List<SBGAssignment> assignments) {
+        this.assignments = assignments;
+    }
 }
