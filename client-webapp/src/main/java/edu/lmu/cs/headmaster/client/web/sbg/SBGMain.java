0604109859c53b723c134c3c4858a292224006fd
Eric Jaso <ericjaso@gmail.com>
2013-05-02 09:47:25 -0700

    Some organization of SBG .java files and html files in client.

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SBGMain.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SBGMain extends SBGPage{
+    
+    public SBGMain(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SchemaCreationPage.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SchemaCreationPage extends SBGPage {
+
+    public SchemaCreationPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
