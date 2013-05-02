450c2bd7c07d4f00b4c4bb6060a10191bcc57cb4
Eric Jaso <ericjaso@gmail.com>
2013-05-02 09:56:42 -0700

    Stubs of HTML and java files for managing Schemas.

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SchemaEditorPage.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SchemaEditorPage extends SBGPage {
+
+    public SchemaEditorPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SchemaWriterPage.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SchemaWriterPage extends SBGPage {
+
+    public SchemaWriterPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
