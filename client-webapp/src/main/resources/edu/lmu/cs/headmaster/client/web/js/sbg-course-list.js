e1563ca7b5b8c033f28d70855527542e7795dd7f
Eric Jaso <ericjaso@gmail.com>
2013-05-08 06:52:39 -0700

    SBGCourseListPage.html now shows your managed courses, based off of student list pages.

--- client-webapp/src/main/resources/edu/lmu/cs/headmaster/client/web/js/sbg-course-list.js
+++ client-webapp/src/main/resources/edu/lmu/cs/headmaster/client/web/js/sbg-course-list.js
@@ -17,4 +17,24 @@ $(function () {
             }
         }
     });
+    
+    Headmaster.loadJsonArrayIntoTable(
+            Headmaster.serviceUri("users/self/courses"),
+            "course-list-progress",
+            "course-list",
+            "course-list-empty",
+    
+        function (course) {
+            return $("<tr></tr>")
+                .append($("<td></td>").text(course.title))
+                .append($("<td></td>").text(course.year))
+                .append($("<td></td>").append(course.term))
+                .click(function () {
+                    location = course.id;
+            });
+        },
+        
+        null,
+        null
+    );
 })
