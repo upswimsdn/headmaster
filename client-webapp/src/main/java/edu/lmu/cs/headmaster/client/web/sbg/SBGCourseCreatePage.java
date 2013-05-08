8b89f21b3b0f3e2920cff6744f3f05fcc3f5a1fa
Eric Jaso <ericjaso@gmail.com>
2013-05-08 03:29:40 -0700

    Added SBGCourseCreatePage.java.

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SBGCourseCreatePage.java
@@ -0,0 +1,10 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SBGCourseCreatePage extends SBGPage {
+
+    public SBGCourseCreatePage(PageParameters pageParameters) {
+        super(pageParameters);
+    }
+}
