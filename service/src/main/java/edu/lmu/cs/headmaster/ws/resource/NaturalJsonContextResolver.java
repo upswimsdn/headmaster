d98590c88b6ce3aae9c38a167a9fcf2e5f36b8c0
Eric Jaso <ericjaso@gmail.com>
2013-05-08 04:51:40 -0700

    Added remaining classes to NaturalJsonContextResolver.java.

--- service/src/main/java/edu/lmu/cs/headmaster/ws/resource/NaturalJsonContextResolver.java
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/resource/NaturalJsonContextResolver.java
@@ -10,6 +10,7 @@ import javax.xml.bind.JAXBContext;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
 
+import edu.lmu.cs.headmaster.ws.domain.Course;
 import edu.lmu.cs.headmaster.ws.domain.Event;
 import edu.lmu.cs.headmaster.ws.domain.GPA;
 import edu.lmu.cs.headmaster.ws.domain.Grant;
@@ -18,6 +19,13 @@ import edu.lmu.cs.headmaster.ws.domain.Student;
 import edu.lmu.cs.headmaster.ws.domain.StudentRecord;
 import edu.lmu.cs.headmaster.ws.domain.User;
 import edu.lmu.cs.headmaster.ws.domain.UserRole;
+import edu.lmu.cs.headmaster.ws.domain.sbg.Assignment;
+import edu.lmu.cs.headmaster.ws.domain.sbg.AssignmentFeedback;
+import edu.lmu.cs.headmaster.ws.domain.sbg.AssignmentRecord;
+import edu.lmu.cs.headmaster.ws.domain.sbg.Objective;
+import edu.lmu.cs.headmaster.ws.domain.sbg.Outcome;
+import edu.lmu.cs.headmaster.ws.domain.sbg.OutcomeEvaluation;
+import edu.lmu.cs.headmaster.ws.domain.sbg.Rubric;
 import edu.lmu.cs.headmaster.ws.types.Role;
 import edu.lmu.cs.headmaster.ws.types.Term;
 
@@ -40,6 +48,14 @@ public class NaturalJsonContextResolver implements ContextResolver<JAXBContext> 
         Role.class,
         Student.class,
         StudentRecord.class,
+        Course.class,
+        Assignment.class,
+        AssignmentFeedback.class,
+        AssignmentRecord.class,
+        Objective.class,
+        Outcome.class,
+        OutcomeEvaluation.class,
+        Rubric.class,
         Term.class,
         User.class,
         UserRole.class
