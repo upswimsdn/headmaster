7d496a282d2fc769ae15da822c108a018a1f4ec1
Eric Jaso <ericjaso@gmail.com>
2013-05-08 02:47:20 -0700

    Added SBGCourseSearchPage.java.

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/sbg/SBGCourseSearchPage.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web.sbg;
+
+import org.apache.wicket.PageParameters;
+
+public class SBGCourseSearchPage extends SBGPage {
+
+    public SBGCourseSearchPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
